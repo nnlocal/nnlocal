66ef6621c7ac9319e1e7a129395ef289d918a471
tramonta <francesco.tramontano@cern.ch>
2025-03-13 09:18:44 +0100

    Small fix in trimerr.sh

--- bin/testrun-H/trimerr.sh
+++ bin/testrun-H/trimerr.sh
@@ -4,7 +4,7 @@
 # the first argument is the number of the entries to skip from above
 # while the second count the entries to skip from below
 (echo 1 $1 $2 ; ls -c1 ????-????.gnu ; echo "") | ../mergedata
-mv fort.12
+rm fort.12
 mv fort.13 nnlocal-$1-$2.top
 exit
 
